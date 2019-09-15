e64de806848993a3321999d1eed444d672ae975e
zhou <624061566@qq.com>
2019-09-15 23:04:36 +0800

    not understand

--- algo_part1/homework/week3/BruteCollinearPoints.java
+++ algo_part1/homework/week3/BruteCollinearPoints.java
@@ -1,5 +1,42 @@
 public class BruteCollinearPoints {
-    public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
-    public           int numberOfSegments()        // the number of line segments
-    public LineSegment[] segments()                // the line segments
+    private int lineNum;
+    private LineSegment[] lineSegment;
+
+    public BruteCollinearPoints(Point[] points){
+        lineNum = 0;
+        if(points == null) throw new IllegalArgumentException();
+        int N = points.length;
+        for (int i = 0; i < N-3; i++){
+            Point p0 = points[i];
+            for(int j = i+1; j < N-2; j++){
+                Point p1 = points[j];
+                double slope0to1 = p0.slopeTo(p1);
+                if(slope0to1 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
+                for (int k = j+1; k < N-1; k++){
+                    Point p2 = points[k];
+                    double slope0to2 = p0.slopeTo(p2);
+                    if(slope0to2 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
+
+                    //如果两个点的斜率相等才考虑进入第四个循环
+                    if(Double.compare(slope0to1,slope0to2) == 0){
+                        for (int w = k+1;w < N; w++){
+                            Point p3 = points[w];
+                            double slope0to3 = p0.slopeTo(p3);
+                            if(slope0to3 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
+
+                            if(Double.compare(slope0to1,slope0to3) == 0){
+                                lineSegment[lineNum++] = new LineSegment(p0,p3);
+                            }else continue;
+                        }
+                    } else continue;
+                }
+            }
+        }
+    }    // finds all line segments containing 4 points
+    public           int numberOfSegments(){
+        return lineNum;
+    }        // the number of line segments
+    public LineSegment[] segments(){
+        return lineSegment;
+    }                // the line segments
 }
--- algo_part1/homework/week3/FastCollinearPoints.java
+++ algo_part1/homework/week3/FastCollinearPoints.java
@@ -1,5 +1,94 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Quick3way;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FastCollinearPoints {
-    public FastCollinearPoints(Point[] points)     // finds all line segments containing 4 or more points
-    public           int numberOfSegments()        // the number of line segments
-    public LineSegment[] segments()                // the line segments
+    private LineSegment[] segments;
+    private int n;
+    public FastCollinearPoints(Point[] points){
+        if(points == null) throw new IllegalArgumentException();
+
+        int N = points.length;
+
+        Point[] aux = new Point[points.length];
+        // 检查每一个元素是否为null
+        for (int i = 0; i < N; i++){
+            if(points[i] == null)
+                throw new IllegalArgumentException();
+            aux[i] = points[i];
+        }
+
+//        // 检查有没有重复元素
+//        for(int i = 0; i < N; i++){
+//            for(int j = i+1; j < N; j++){
+//                if(points[i].slopeTo(points[j]) == 0){
+//                    throw new IllegalArgumentException();
+//                }
+//            }
+//        }
+
+        Arrays.sort(aux);
+        ArrayList<LineSegment> lineSegments = new ArrayList<>();
+
+        Point prev = null;
+
+        for (int i = 0; i < N; i++){
+            Point p = aux[i];
+
+            if(prev != null && p.compareTo(prev) == 0){
+                throw new IllegalArgumentException();
+            }else{
+                prev = p;
+            }
+
+            Point[] slopeOrder = aux.clone();
+            Arrays.sort(aux,p.slopeOrder());
+
+
+            double lastSlope = Double.NEGATIVE_INFINITY;
+            int slopeStartIndex = 0;
+
+            for(int j = 1; j < N; j++){
+                Point q = slopeOrder[j];
+                double curSlope = p.slopeTo(q);
+
+                boolean isLastPoint = j == N-1;
+
+                if(Double.compare(curSlope,lastSlope) != 0){
+                    if(j - slopeStartIndex >= 3){
+                        if(p.compareTo(slopeOrder[slopeStartIndex]) <= 0){
+                            LineSegment segment = new LineSegment(p, slopeOrder[j-1]);
+                            lineSegments.add(segment);
+                        }
+                    }
+
+                    slopeStartIndex = j;
+
+                }else if (isLastPoint){
+                    if(j - slopeStartIndex >= 2){
+                        if(p.compareTo(slopeOrder[slopeStartIndex]) <= 0){
+                            LineSegment segment = new LineSegment(p,q);
+                            lineSegments.add(segment);
+                        }
+                    }
+                }
+
+                lastSlope = curSlope;
+            }
+        }
+
+        n = lineSegments.size();
+        for(int i = 0; i < n; i++){
+            segments[i] = lineSegments.get(i);
+        }
+
+    }     // finds all line segments containing 4 or more points
+    public           int numberOfSegments(){
+        return segments.length;
+    }        // the number of line segments
+    public LineSegment[] segments(){
+        return segments.clone();
+    }                // the line segments
 }
