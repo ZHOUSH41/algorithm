c4a6cc162318fbcb11ed5b643c23ead956f033cf
zhou <624061566@qq.com>
2019-09-16 11:12:36 +0800

    score 97

--- algo_part1/homework/week3/BruteCollinearPoints.java
+++ algo_part1/homework/week3/BruteCollinearPoints.java
@@ -3,6 +3,7 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,29 +19,57 @@ public class BruteCollinearPoints {
 
         // 使用链表结构
         List<LineSegment> lineSegments = new LinkedList<LineSegment>();
+        Point[] aux = new Point[points.length];
+
+        for(int i = 0; i < N; i++){
+            if(points[i] == null) throw new IllegalArgumentException();
+            aux[i] = points[i];
+        }
+
+        Arrays.sort(aux);
+
+        Point prev = null;
+
         for (int i = 0; i < N-3; i++){
-            Point p0 = points[i];
+            //Point p0 = points[i];
+            if(prev != null && prev.compareTo(aux[i]) == 0){
+                throw new IllegalArgumentException();
+            }else {
+                prev = aux[i];
+            }
+
             for(int j = i+1; j < N-2; j++){
-                Point p1 = points[j];
-                double slope0to1 = p0.slopeTo(p1);
-                if(slope0to1 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
+                //Point p1 = points[j];
+                //double slope0to1 = p0.slopeTo(p1);
+                //if(slope0to1 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
                 for (int k = j+1; k < N-1; k++){
-                    Point p2 = points[k];
-                    double slope0to2 = p0.slopeTo(p2);
-                    if(slope0to2 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
-
-                    //如果两个点的斜率相等才考虑进入第四个循环
-                    if(Double.compare(slope0to1,slope0to2) == 0){
-                        for (int w = k+1;w < N; w++){
-                            Point p3 = points[w];
-                            double slope0to3 = p0.slopeTo(p3);
-                            if(slope0to3 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
-
-                            if(Double.compare(slope0to1,slope0to3) == 0){
-                                lineSegments.add(new LineSegment(p0, p3));
-                            }else continue;
+                    for (int w = k+1; w < N; w++){
+                        Point p0 = aux[i];
+                        Point p1 = aux[j];
+                        Point p2 = aux[k];
+                        Point p3 = aux[w];
+                        if(Double.compare(p0.slopeTo(p1),p0.slopeTo(p2)) == 0 &&
+                        Double.compare(p0.slopeTo(p2),p0.slopeTo(p3)) == 0){
+                            LineSegment segment = new LineSegment(p0,p3);
+                            lineSegments.add(segment);
                         }
                     }
+                    //Point p2 = points[k];
+                    //double slope0to2 = p0.slopeTo(p2);
+                    //if(slope0to2 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
+
+//                    //如果两个点的斜率相等才考虑进入第四个循环
+//                    if(Double.compare(slope0to1,slope0to2) == 0){
+//                        for (int w = k+1;w < N; w++){
+//                            Point p3 = points[w];
+//                            double slope0to3 = p0.slopeTo(p3);
+//                            if(slope0to3 == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
+//
+//                            if(Double.compare(slope0to1,slope0to3) == 0){
+//                                lineSegments.add(new LineSegment(p0, p3));
+//                            }else continue;
+//                        }
+//                    }
                 }
             }
         }
