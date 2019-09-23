0b1e911bdc8cd9a89e2a4c97eaabde00567cbb27
zhou <624061566@qq.com>
2019-09-23 21:36:51 +0800

    pointSet

--- algo_part1/homework/week5/PointSET.java
+++ algo_part1/homework/week5/PointSET.java
@@ -1,15 +1,74 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
 
 public class PointSET {
-    public         PointSET()                               // construct an empty set of points
-    public           boolean isEmpty()                      // is the set empty?
-    public               int size()                         // number of points in the set
-    public              void insert(Point2D p)              // add the point to the set (if it is not already in the set)
-    public           boolean contains(Point2D p)            // does the set contain point p?
-    public              void draw()                         // draw all points to standard draw
-    public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle (or on the boundary)
-    public           Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
+    private final SET<Point2D>  points;
 
-    public static void main(String[] args)
+    public PointSET(){
+        points = new SET<Point2D>();
+    }                              // construct an empty set of points
+
+    public boolean isEmpty(){
+        return points.isEmpty();
+    }                      // is the set empty?
+
+    public int size(){
+        return points.size();
+    }                         // number of points in the set
+
+    public void insert(Point2D p){
+        if (p == null) throw new IllegalArgumentException();
+
+        points.add(p);
+    }              // add the point to the set (if it is not already in the set)
+
+    public boolean contains(Point2D p){
+        if (p == null) throw new IllegalArgumentException();
+
+        return points.contains(p);
+    }            // does the set contain point p?
+
+    public void draw(){
+        for (Point2D p : points){
+            p.draw();
+        }
+    }                         // draw all points to standard draw
+
+    public Iterable<Point2D> range(RectHV rect){
+        if (rect == null) throw new IllegalArgumentException();
+        SET<Point2D> rangePoints = new SET<Point2D>();
+        for (Point2D p : points){
+            if (rect.contains(p)){
+                rangePoints.add(p);
+            }
+        }
+
+        return rangePoints;
+    }             // all points that are inside the rectangle (or on the boundary)
+
+    public Point2D nearest(Point2D p){
+        if (p == null) throw new IllegalArgumentException();
+
+        if (isEmpty()) return null;
+
+        Point2D nearest = null;
+
+        for (Point2D point : points){
+            if (nearest == null){
+                nearest = point;
+                continue;
+            }
+
+            if (p.distanceSquaredTo(point) < p.distanceSquaredTo(nearest)){
+                nearest = point;
+            }
+        }
+
+        return nearest;
+    }             // a nearest neighbor in the set to point p; null if the set is empty
+
+    public static void main(String[] args){
+
+    }
 }
